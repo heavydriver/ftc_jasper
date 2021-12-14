7499f7a5fed2abf8cd9e53c7ad31409893252b13
Khosraw Azizi <53713571+Khosraw@users.noreply.github.com>
2021-12-13 18:18:10 -0600

    moveforward and backward method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import java.lang.Thread;
 
 @TeleOp(name = "Test")
 public class Test extends LinearOpMode {
@@ -21,18 +22,47 @@ public class Test extends LinearOpMode {
         leftCarousel = hardwareMap.get(DcMotor.class, "leftCarousel");
 
         waitForStart();
-
+        
         if (opModeIsActive()) {
             // vertical = -gamepad1.left_stick_y;
 
             while (opModeIsActive()) {
-                rightBack.setPower(1);
-                leftBack.setPower(1);
+
+                
                 leftCarousel.setPower(1);
+                moveForward(rightBack, leftBack, 1000);
+                moveBackward(rightBack, leftBack, 1000);
                 
 
                 telemetry.update();
             }
         }
+    }
+
+    // time in miliseconds
+    public void moveForward(DcMotor rightBack, DcMotor leftBack, int time) {
+            leftBack.setDirection(DcMotor.Direction.REVERSE);
+
+            rightBack.setPower(1);
+            leftBack.setPower(1);
+            Thread.sleep(time);
+
+            rightBack.setPower(0);
+            leftBack.setPower(0);
+
+            leftBack.setDirection(DcMotor.Direction.FORWARD);
+    }
+
+    public void moveBackward(DcMotor rightBack, DcMotor leftBack, int time) {
+            rightBack.setDirection(DcMotor.Direction.REVERSE);
+
+            rightBack.setPower(1);
+            leftBack.setPower(1);
+            Thread.sleep(time);
+
+            rightBack.setPower(0);
+            leftBack.setPower(0);
+
+            rightBack.setDirection(DcMotor.Direction.FORWARD);
     }
 };
